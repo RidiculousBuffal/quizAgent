38f5ee00e14d9527ee1f39bbc3583c9e6ee8f25f
RidiculousBuffal <3322946829@qq.com>
2025-03-19 18:51:14 +0800

    Initial commit

---
+++ src/main/java/com/dhu/dhusoftware/DhuSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.dhu.dhusoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhuSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhuSoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhu/dhusoftware/DhuSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhu.dhusoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhuSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
