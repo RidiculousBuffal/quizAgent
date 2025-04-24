631c2d0f0c73b70f80f3efecdb810b1d88be2586
haoranTu <1642804440@qq.com>
2025-04-24 13:09:42 +0800

    fit:修复跨域请求问题，options预检请求拦截问题，satoken无法获取的问题

--- src/main/java/com/dhu/dhusoftware/config/WebCorsConfig.java
+++ src/main/java/com/dhu/dhusoftware/config/WebCorsConfig.java
@@ -1,25 +1,17 @@
 package com.dhu.dhusoftware.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNullApi;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebCorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 允许所有接口
-                        .allowedOrigins("http://localhost:13144") // 你前端的地址，支持多个
-                        .allowedMethods("*")
-                        .allowedHeaders("*") // 允许所有请求头
-                        .allowCredentials(true); // 是否允许携带cookie
-            }
-        };
+public class WebCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:13144")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
--- src/main/java/com/dhu/dhusoftware/config/WebMvcConfig.java
+++ src/main/java/com/dhu/dhusoftware/config/WebMvcConfig.java
@@ -1,8 +1,15 @@
 package com.dhu.dhusoftware.config;
 
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.context.model.SaRequest;
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,8 +17,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+        registry.addInterceptor(new SaInterceptor(handle -> {
+                    // 拿到当前请求的 SaRequest
+                    SaRequest request = SaHolder.getRequest();
+                    // 跳过预检
+                    if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+                        return;
+                    }
+                    // 其余请求做登录校验
+                    SaRouter.match("/**")
+                            .notMatch("/login")
+                            .check(r -> StpUtil.checkLogin());
+                }))
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login");
+                .order(1);
     }
 }
