bc98e1923b586710bbd5a55abb92c3a146e748dd
RidiculousBuffal <3322946829@qq.com>
2025-04-26 13:14:21 +0800

    把satoken 等通用头移动到base中

--- frontend/src/api/quizApi.ts
+++ frontend/src/api/quizApi.ts
@@ -1,14 +1,9 @@
-import { useUserStore } from "../store/user/UserStore.ts";
 import { fetchAPI } from "./base.ts";
 
 export function createQuiz(quizName: string, description: string, quizStartTime: string, quizEndTime: string) {
-    const satoken = useUserStore.getState().saToken ?? '';
+
     const response = fetchAPI('/api/quiz/save', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'satoken': `${satoken}`
-        },
         body: JSON.stringify({
             quizId: null,
             quizName: quizName,
