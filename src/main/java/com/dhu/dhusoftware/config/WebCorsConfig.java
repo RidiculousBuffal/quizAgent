a817fe0499b7007c4e328640b3267bb736dc5607
RidiculousBuffal <3322946829@qq.com>
2025-05-06 14:54:17 +0800

    移除跨域限制

--- src/main/java/com/dhu/dhusoftware/config/WebCorsConfig.java
+++ src/main/java/com/dhu/dhusoftware/config/WebCorsConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableConfigurationProperties(LogtoConfig.class)
 public class WebCorsConfig implements WebMvcConfigurer {
-    @Autowired
-    private LogtoConfig logtoConfig;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns(this.logtoConfig.getFrontendUrl())
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    @Autowired
+//    private LogtoConfig logtoConfig;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns(this.logtoConfig.getFrontendUrl())
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 }
--- src/main/java/com/dhu/dhusoftware/config/WebMvcConfig.java
+++ src/main/java/com/dhu/dhusoftware/config/WebMvcConfig.java
@@ -2,11 +2,14 @@ package com.dhu.dhusoftware.config;
 
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.context.model.SaRequest;
+import cn.dev33.satoken.fun.strategy.SaCorsHandleFunction;
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -15,6 +18,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public SaCorsHandleFunction corsHandle() {
+        return (req, res, sto) -> {
+            res.
+                    // 允许指定域访问跨域资源
+                     setHeader("Access-Control-Allow-Origin", "*")
+                    // 允许所有请求方式
+                    .setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+                    // 有效时间
+                    .setHeader("Access-Control-Max-Age", "3600")
+                    // 允许的header参数
+                    .setHeader("Access-Control-Allow-Headers", "*");
+
+            // 如果是预检请求，则立即返回到前端
+            SaRouter.match(SaHttpMethod.OPTIONS)
+                    .free(r -> System.out.println("--------OPTIONS预检请求，不做处理"))
+                    .back();
+        };
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 1. 放行所有 OPTIONS 预检请求
