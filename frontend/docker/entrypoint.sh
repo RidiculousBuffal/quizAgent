fb6b3d23ff6cf334ffc27f2cb217317ac4e5a604
haoranTu <1642804440@qq.com>
2025-05-08 23:30:29 +0800

    fix:docker env.js syntax error

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -8,4 +8,5 @@ window.__APP_ENV__ = {
   VITE_APP_LOGTO_APPID:             "${VITE_APP_LOGTO_APPID:-}",
   VITE_APP_URL:                     "${VITE_APP_URL:-}",
   VITE_APP_API_URL:                 "${VITE_APP_API_URL:-}",
-  VITE_APP_LLAMA_INDEX_BASE_URL:    "${VITE_APP_LLAMA_INDEX_BASE_URL:-}",
+  VITE_APP_LLAMA_INDEX_BASE_URL:    "${VITE_APP_LLAMA_INDEX_BASE_URL:-}"
+}
